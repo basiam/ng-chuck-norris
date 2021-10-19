e040316bf2f8db37e4fb025a676f3101d56ad798
Basia Madej <barbara.madej@gmail.com>
2021-10-19 12:33:27 +0200

    Cleanup generated code, added eslint

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/ng-chuck-norris'));
 
-app.get('/*', function(req,res) {
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname+'/dist/ng-chuck-norris/index.html'));
 });
 
