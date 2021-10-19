e5ba48cb6300e3b97bff1345dd294578eddaed31
Basia Madej <barbara.madej@gmail.com>
2021-10-19 14:16:46 +0200

    Fetching 5 jokes from API

--- src/app/jokes/jokes.component.ts
+++ src/app/jokes/jokes.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 import { Joke } from './joke.model';
 import { JokesService } from './jokes.service';
@@ -8,12 +10,23 @@ import { JokesService } from './jokes.service';
   templateUrl: './jokes.component.html',
   styleUrls: ['./jokes.component.scss']
 })
-export class JokesComponent implements OnInit {
+export class JokesComponent implements OnInit, OnDestroy {
   jokes: Joke[] = [];
-  constructor(private jokeService: JokesService) { }
+  subscription: Subscription;
+  constructor(private jokesService: JokesService, private router: Router) { }
 
   ngOnInit(): void {
-    this.jokes = this.jokeService.getJokes()
+
+    this.subscription = this.jokesService.jokesLoaded.subscribe(
+      (jokes: Joke[]) => {
+        this.jokes = jokes;
+      }
+    );
+    this.jokesService.fetchJokes(this.router.url.split('/')[1]);
+
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
