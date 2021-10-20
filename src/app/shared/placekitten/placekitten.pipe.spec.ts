4d3096990650af45788a784e5363b156675f753e
Basia Madej <barbara.madej@gmail.com>
2021-10-20 19:05:52 +0200

    Cleanup and removing generated spec

--- src/app/shared/placekitten/placekitten.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PlacekittenPipe } from './placekitten.pipe';
-
-describe('PlacekittenPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PlacekittenPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
